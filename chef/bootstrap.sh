0f2d66b9af62c84294ea047f5c6abee08b935bd5
SudoBasher <sudobasher@users.noreply.github.com>
2017-05-11 19:08:38 -0700

    Merging previous commit changes into this branch

--- chef/bootstrap.sh
+++ chef/bootstrap.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 mkdir -p /home/user/Downloads/Packages/
-wget https://packages.chef.io/files/stable/chef/12.19.36/ubuntu/16.04/chef_12.19.36-1_amd64.deb /home/user/Downloads/Packages/chef_12.19.36-1_amd64.deb
+wget https://packages.chef.io/files/stable/chef/12.19.36/ubuntu/16.04/chef_12.19.36-1_amd64.deb -O /home/user/Downloads/Packages/chef_12.19.36-1_amd64.deb
 sudo dpkg -i /home/user/Downloads/Packages/chef_12.19.36-1_amd64.deb
 wget https://raw.githubusercontent.com/SudoBasher/builds/master/chef/ubuntu-development-workstation.rb -O /home/user/Downloads/Packages/ubuntu-development-workstation.rb 
 sudo chef-client --local-mode /home/user/Downloads/Packages/ubuntu-development-workstation.rb
