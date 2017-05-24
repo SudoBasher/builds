4e26c54ea90151265cedc5c725db30ac66fa5124
SudoBasher <email.ca@gmail.com>
2017-05-23 23:52:01 -0700

    Adding deb package downloads and installs.
    Organized apt install package lists a bit.

--- chef/ubuntu-development-workstation.rb
+++ chef/ubuntu-development-workstation.rb
@@ -1,4 +1,4 @@
-# install os
+# install ubuntu gnome x64 os
 
 # execute 'Remove sudo password' do
   # command 'sudo echo "user ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers'
@@ -17,19 +17,33 @@ execute 'Upgrade packages' do
 end
 
 package [
-  'git',
-  'htop',
-  'gparted',
-  'nemo',
-  'silversearcher-ag',
-  'openvpn',
-  'ufw',
-  'sysinfo',
-  'nmap',
-  'ruby-bundler',
-  'gimp',
-  'nfs-common',
-  'nfs-kernel-server'
+  # general packages
+    # dev
+      'git',
+      'ruby-bundler',
+      'python-pip',
+    # utilities
+      'htop',
+      'silversearcher-ag',
+    # network
+      'openvpn',
+      'ufw',
+      'sysinfo',
+      'nmap',
+    # av
+      'gimp',
+    # office
+      'libreoffice',
+    # file systems / volume management
+      'nemo',
+      'gparted',
+      'nfs-common',
+      'nfs-kernel-server',
+  # package dependecies
+    # gnome system-monitor
+      'gir1.2-gtop-2.0',
+      'gir1.2-networkmanager-1.0',
+      'gir1.2-clutter-1.0',
 ]
 
 execute 'Start firewall' do
@@ -49,15 +63,40 @@ dpkg_package '/home/user/Downloads/Packages/sublime-text_build-3126_amd64.deb' d
   action :install
 end
 
+remote_file '/home/user/Downloads/Packages/google-chrome-stable_current_amd64.deb' do
+  action :create
+  source 'https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb'
+end
+
+dpkg_package '/home/user/Downloads/Packages/google-chrome-stable_current_amd64.deb' do
+  action :install
+  options '--install-suggests'
+end
+
+remote_file '/home/user/Downloads/Packages/slack-desktop-2.6.0-amd64.deb' do
+  action :create
+  source 'https://downloads.slack-edge.com/linux_releases/slack-desktop-2.6.0-amd64.deb'
+end
+
+dpkg_package '/home/user/Downloads/Packages/slack-desktop-2.6.0-amd64.deb' do
+  action :install
+end
+
+remote_file '/home/user/Downloads/Packages/gitkraken-amd64.deb' do
+  action :create
+  source 'https://release.gitkraken.com/linux/gitkraken-amd64.deb'
+end
+
+dpkg_package '/home/user/Downloads/Packages/gitkraken-amd64.deb' do
+  action :install
+end
+
 ##### figure out how to do this
 
 ### install
-# pip
-# sublime text
 # zoom
 # cisco anyconnect
 # veracrypt
-# google chrome
 # google earth
 # opera
 # ghostery on firefox
@@ -67,7 +106,11 @@ end
 # nemo
 # blank screen / screensaver
 
+### registration
+# sublime text
+
 ### configure
+# sublime text
 # terminal preferences
-# ssh key
+# ssh key(s)
 # background image
