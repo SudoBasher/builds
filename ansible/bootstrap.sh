eabb6a89910e08640df1a4ca67cad1fb8382f018
SudoBasher <email.ca@gmail.com>
2017-10-12 17:57:37 -0700

    Correcting some links.

--- ansible/bootstrap.sh
+++ ansible/bootstrap.sh
@@ -8,7 +8,7 @@
 mkdir -p /home/user/Downloads/Packages/
 sudo chown -R user:user /home/user/Downloads/Packages/
 mkdir -p /home/user/Development/github/sudobasher/builds.local/ansible/
-wget https://raw.githubusercontent.com/SudoBasher/builds.local/master/ansible/ubuntu-development-workstation.yml -O /home/user/Development/github/sudobasher/builds.local/ansible/ubuntu-development-workstation.yml
+wget https://raw.githubusercontent.com/SudoBasher/builds/master/ansible/ubuntu-development-workstation.yml -O /home/user/Development/github/sudobasher/builds.local/ansible/ubuntu-development-workstation.yml
 sudo chown -R user:user /home/user/Development/
 apt-get -y update
 apt-get install software-properties-common
