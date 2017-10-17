5672289997eb1a432347183ff9cd428341412f1c
SudoBasher <email.ca@gmail.com>
2017-10-17 09:21:08 -0700

    Adding home printers, password generator, and small bootstrap edits.

--- ansible/bootstrap.sh
+++ ansible/bootstrap.sh
@@ -11,8 +11,8 @@ mkdir -p /home/user/Development/github/sudobasher/builds.local/ansible/
 wget https://raw.githubusercontent.com/SudoBasher/builds/master/ansible/ubuntu-development-workstation.yml -O /home/user/Development/github/sudobasher/builds.local/ansible/ubuntu-development-workstation.yml
 sudo chown -R user:user /home/user/Development/
 apt-get -y update
-apt-get install software-properties-common
-apt-add-repository ppa:ansible/ansible
+apt-get -y install software-properties-common
+apt-add-repository -y ppa:ansible/ansible
 apt-get -y update
 apt-get -y install ansible
 ansible-playbook /home/user/Development/github/sudobasher/builds.local/ansible/ubuntu-development-workstation.yml 
