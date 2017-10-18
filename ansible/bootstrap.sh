421fa1dda851e0642b8ff88d5c79e854ce7ea713
SudoBasher <email.ca@gmail.com>
2017-10-18 15:13:40 -0700

    More cleanup.

--- ansible/bootstrap.sh
+++ ansible/bootstrap.sh
@@ -8,13 +8,12 @@
 export USERNAME=user ## set this to your login username
 mkdir -p /home/${USERNAME}/Downloads/Packages/
 sudo chown -R ${USERNAME}:${USERNAME} /home/${USERNAME}/Downloads/Packages/
-mkdir -p /home/${USERNAME}/Development/github/sudobasher/builds.local/ansible/
 apt -y install git
-git clone http://git@github.com/sudobasher/builds/ /home/${USERNAME}/Development/github/sudobasher/builds.local/
+git clone http://git@github.com/sudobasher/builds/ /home/${USERNAME}/Development/github/sudobasher/builds/
 sudo chown -R ${USERNAME}:${USERNAME} /home/${USERNAME}/Development/
 apt -y update
 apt -y install software-properties-common
 apt-add-repository -y ppa:ansible/ansible
 apt -y update
 apt -y install ansible
-ansible-playbook /home/${USERNAME}/Development/github/sudobasher/builds.local/ansible/ubuntu-development-workstation.yml 
+ansible-playbook /home/${USERNAME}/Development/github/sudobasher/builds/ansible/ubuntu-development-workstation.yml 
